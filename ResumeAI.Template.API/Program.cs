fc08e9908322e85fc94007fd0e9eb947e0b6bbcf
Abhaysingh358 <itsabhaypratapsingh03@gmail.com>
2026-05-13 03:51:45 +0530

    [Abhay] Refactored.Template.API/Program.cs

--- ResumeAI.Template.API/Program.cs
+++ ResumeAI.Template.API/Program.cs
@@ -51,15 +51,15 @@ try
 
     app.UseMiddleware<GlobalExceptionMiddleware>();
 
-    if (app.Environment.IsDevelopment())
-    {
+    // if (app.Environment.IsDevelopment())
+    // {
         app.UseSwagger();
         app.UseSwaggerUI(c =>
         {
             c.SwaggerEndpoint("/swagger/v1/swagger.json", "ResumeAI Template Service v1");
             c.RoutePrefix = string.Empty;
         });
-    }
+    // }
 
     app.UseSerilogRequestLogging();
     app.UseHttpsRedirection();
